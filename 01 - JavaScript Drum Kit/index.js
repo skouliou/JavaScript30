1dc379ed3d66c761d4360c2d0c6a6bfbf780c3ee
Imed Eddine Nezzar <imed.eddine.nezzar@gmail.com>
2024-11-22 12:39:21 +0100

    day 1: JS Drum Kit

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio || !key) {
+    console.error("audio or key not found!");
+    return;
+  }
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+});
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove('playing');
+}
